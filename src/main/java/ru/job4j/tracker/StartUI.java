f96140b094e786b5ed4d6b0a12773ab2cde40a2f
Jagupov Jurii <project@idefclub.ru>
2021-03-12 12:57:19 +0300

    7. toString [#384140]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item obj = new Item();
+        System.out.println(obj);
+    }
+}
