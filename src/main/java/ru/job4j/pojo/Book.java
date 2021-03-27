aa93c3f671c3a2ba48f94ab6462af6c90f38bd72
Jagupov Jurii <project@idefclub.ru>
2021-03-27 03:12:03 +0300

    fix Массивы и модели. [#384145]

--- src/main/java/ru/job4j/pojo/Book.java
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -4,17 +4,16 @@ public class Book {
     private String name;
     private int pageCount;
 
+    public Book(String name, int pageCount) {
+        this.name = name;
+        this.pageCount = pageCount;
+    }
+
     public int getPageCount() {
         return pageCount;
     }
 
     public void setPageCount(int pageCount) {
-        this.pageCount = pageCount;
-    }
-
-
-    public Book(String name, int pageCount) {
-        this.name = name;
         this.pageCount = pageCount;
     }
 
