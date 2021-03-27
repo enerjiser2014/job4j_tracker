da0f516747e29c252a4189dd197441d52bf9ac7e
Jagupov Jurii <project@idefclub.ru>
2021-03-27 03:13:53 +0300

    fix Массив с пустыми ячейками [#384147]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -16,7 +16,9 @@ public class Shop {
     }
     public static int indexOfNull(Product[] products){
         for (int i = 0; i < products.length; i++) {
-            if (products[i] == null) return i;
+            if (products[i] == null) {
+                return i;
+            }
         }
         return -1;
     }
