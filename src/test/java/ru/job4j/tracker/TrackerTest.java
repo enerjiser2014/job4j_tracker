a865b3b379773c061b67963364cb59ce8e46e26e
Jagupov Jurii <project@idefclub.ru>
2021-04-11 04:47:37 +0300

    Метод замены заявки. Tracker.replace [#384148]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -24,7 +24,6 @@ public class TrackerTest {
         int id = bug.getId();
         Item bugWithDesc = new Item();
         bugWithDesc.setName("Bug with description");
-
         tracker.replace(id, bugWithDesc);
         assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
