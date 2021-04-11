1380fd87917f14783fca08aa3ee1a4ccdc70a25b
Jagupov Jurii <project@idefclub.ru>
2021-04-11 04:45:40 +0300

    Метод замены заявки. Tracker.replace [#384148]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,20 +7,30 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+            }
+        }
+        return rsl;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item[] findAll(){
+    public Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
 
-    public Item[] findByName(String key){
+    public Item[] findByName(String key) {
         int arrSize = 0;
         Item[] byName = new Item[100];
-        for (int index = 0; index < arrSize; index++){
+        for (int index = 0; index < size; index++) {
             Item el = items[index];
             // el.getname==key
             if (el.getName().equals(key)) {
@@ -31,13 +41,20 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
+        int index = indexOf(id);
+        if (index !=-1) {
+            return items[index];
+        }
+        return null;
+        //return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item){
+        boolean rsl = false;
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = item;
+            rsl = true;
         }
         return rsl;
     }
