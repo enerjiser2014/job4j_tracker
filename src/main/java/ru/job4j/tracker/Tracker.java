be5d213fe5fe73dcd3569b1751d2fb9c485ca0a5
Jagupov Jurii <project@idefclub.ru>
2021-04-11 19:52:33 +0300

    Метод удаления заявки Tracker.delete [#384149]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -62,7 +62,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            System.arraycopy(items, index + 1, items, index, size - index -1);
+            System.arraycopy(items, index + 1, items, index, size - index);
             items[--size] = null;
         }
         return rsl;
