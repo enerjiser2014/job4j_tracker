a542003b21b41a8c7b26d22afec12e06b3023281
Jagupov Jurii <project@idefclub.ru>
2021-04-24 00:09:04 +0300

    Разрыв зависимости StartUI от Scanner. [#384078]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,19 +1,18 @@
 package ru.job4j.tracker;
 
-import java.util.Scanner;
-
 public class StartUI {
 
-    public void init(Scanner scanner, Tracker tracker) {
+    public void init(ConsoleInput input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             System.out.print("Select: ");
-            int select = Integer.parseInt(scanner.nextLine());
+            String msg = "(Здесь может быть ваша реклама)";
+            int select = Integer.parseInt(input.askStr(msg));
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
                 System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr(msg);
                 Item item = new Item(name);
                 tracker.add(item);
             } else if (select == 1) {
@@ -23,9 +22,9 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("Enter task id to change: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr(msg));
                 System.out.println("Enter task name to change: ");
-                String name = scanner.nextLine();
+                String name = input.askStr(msg);
                 Item newItem = new Item(name);
                 if (!tracker.replace(id, newItem)) {
                     System.out.println("Error! can't change");
@@ -34,7 +33,7 @@ public class StartUI {
                 }
             } else if (select == 3) {
                 System.out.println("Enter task id to delete: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr(msg));
                 if (!tracker.delete(id)) {
                     System.out.println("Error! id can't delete");
                 } else {
@@ -43,7 +42,7 @@ public class StartUI {
 
             } else if (select == 4) {
                 System.out.println("Enter task id to find: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr(msg));
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
@@ -52,7 +51,7 @@ public class StartUI {
                 }
             } else if (select == 5) {
                 System.out.println("Enter task name to find: ");
-                String taskName = scanner.nextLine();
+                String taskName = input.askStr(msg);
                 Item items[] = tracker.findByName(taskName);
                 if (items.length > 0) {
                     for (Item item : items) {
@@ -79,8 +78,8 @@ public class StartUI {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        ConsoleInput input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
+        new StartUI().init(input, tracker);
     }
 }
