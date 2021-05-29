1d34ab2225ceb7e6fa072389a807778d522a96ec
Jagupov Jurii <project@idefclub.ru>
2021-05-29 18:36:01 +0300

    Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -80,4 +80,52 @@ public class StartUITest {
         assertThat(tracker.findAll(), is(items));
     }
 
+    @Test
+    public void whenFindByIdAction() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[3];
+        int count = 0;
+        while (count < items.length) {
+            items[count] = tracker.add(new Item("test"+(count+1)));
+            count++;
+        }
+        Input in = new StubInput(
+                new String[] {"0" , "1"}
+        );
+        UserAction[] actions = {
+                new FindAllAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        // check results
+        int id = 1;
+        Item check = tracker.findById(id);
+        assertThat(check.getId(), is(id));
+    }
+
+    @Test
+    public void whenFindByNameAction() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item[] items = new Item[3];
+        int count = 0;
+        while (count < items.length) {
+            items[count] = tracker.add(new Item("test"+(count+1)));
+            count++;
+        }
+        Input in = new StubInput(
+                new String[] {"0" , "1"}
+        );
+        UserAction[] actions = {
+                new FindAllAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        // check results
+        Item findByName  = tracker.findByName("test1")[0];
+        Item check = tracker.findById(1);
+        assertThat(findByName, is(check));
+    }
+
 }
