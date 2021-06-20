d5a76ace2e6222bbf3e55e48e633e6c8c5744c33
JJ <project@idefclub.ru>
2021-06-21 00:11:18 +0300

    Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calcTestSmallThen0() {
+        int rsl = Fact.calc(-1);
+    }
+}
