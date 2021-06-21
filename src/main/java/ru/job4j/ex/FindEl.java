e24ea9f8a49b6461823d57d902410961a04f06a7
JJ <project@idefclub.ru>
2021-06-21 23:40:56 +0300

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) {
+        int rsl = -1;
+        try {
+            /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
+            for (int index = 0; index < value.length; index++) {
+                if (value.equals(key)) {
+                    rsl = index;
+                    break;
+                }
+            }
+            if (rsl == -1) {
+                throw new ElementNotFoundException("Element not found");
+            }
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+}
