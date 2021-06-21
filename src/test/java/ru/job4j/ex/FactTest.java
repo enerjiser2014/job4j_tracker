c0b1a8103807dccd1dc79d300daf1031ac36d742
JJ <project@idefclub.ru>
2021-06-21 23:51:10 +0300

    fix  Тесты вывода на консоль [#33585 #161940]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -6,6 +6,6 @@ public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void calcTestSmallThen0() {
-        int rsl = Fact.calc(-1);
+        int rsl = new Fact().calc(-1);
     }
 }
