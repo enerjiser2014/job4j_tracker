140d90f7ccd71b5497cbb4fad93495f258532006
JJ <project@idefclub.ru>
2021-06-23 09:29:35 +0300

    Пользовательские исключения. [#219367 #170110]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,15 @@
 package ru.job4j.ex;
 
 public class FindEl {
+    public static void main(String[] args) {
+        String[] v = { "test1", "test2", "test3"};
+        int index;
+        try {
+            index = indexOf(v, "test2");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
             /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
